9831edc85696a3389f17667bc0181a792e6d4e93
Mojtaba Charchian <mojishah70@gmail.com>
2024-03-31 20:17:24 +0330

    db is updated

--- src/foodData.js
+++ src/foodData.js
@@ -136,6 +136,145 @@ let foods = [
         desc: "Sholeh Zard is a famous Iranian dessert with a lovely visual, alluring aroma, and delicious flavour; this Persian saffron rice pudding is bound to give your palate a royal taste and keep you craving more.",
         img: 'Img/Sholeh Zard.jpg'
     },
-]
+
+    {
+        id: 18,
+        name: 'Ashe Reshteh',
+        type: 'Dessert',
+        desc: "Ash is one of the traditional Iranian dishes, which is known as the simplest dish, not so easy to prepare the ingredients though. Ash Reshteh is one of the most common dishes that is cooked in different ways in different cities. Vegetables include spinach, coriander, parsley, dill, and leek, which is sold as Ash vegetable. The amount of beans and chickpeas in Ash is also arbitrary, which is completely a matter of taste. They also add noodles (Reshteh)..",
+        img: 'Img/Ashe-Reshteh.JPG'
+    },
+
+    {
+        id: 19,
+        name: 'Kuku-Sabzi',
+        type: 'Dinner',
+        desc: "Kuku Sabzi and Sibzamini Kuku are some of the Kuku-s that most people like. It is often used for dinner or a snack, and it is very easy to cook. Kuku Sabzi is one of the most popular types of Kuku among Iranians. The most important component of this dish is the vegetables. Every city has its own Kuku Sabzi, for example, Mazandarani people use different vegetables from what Tabrizi people use.",
+        img: 'Img/Kuku-Sabzi.JPG'
+    },
+
+    {
+        id: 20,
+        name: 'Albaloo Polo',
+        type: 'Lunch',
+        desc: "With its beautiful and colorful appearance, cherry (Albaloo) is considered a ceremonial food and is served at many parties and formal events. Albaloo Polo is one of the most delicious Iranian dishes and it is made with both red meat and chicken. Making Albaloo Polo requires special skills and not everyone can make it well.",
+        img: 'Img/Albaloo-Polo.jpg'
+    },
+
+    {
+        id: 21,
+        name: 'Lubia Polo',
+        type: 'Lunch',
+        desc: "Lubia Polo is one of the most delicious Iranian dishes, which has special customs served. Iranians usually eat this food with bread or potato Tahdig, Shirazi salad, vegetables, and yogurt, which is a very delicious combination. To prepare this dish they use rice, chopped green beans combined with ground meat, onion, tomato paste, and various spices and placed it on low heat until it is ready.",
+        img: 'Img/Lubia Polo.JPG'
+    },
+
+    {
+        id: 22,
+        name: 'Dougho Gooshfil',
+        type: 'Dessert',
+        desc: "One of the delicious and famous combinations of Iran, which most people consider to belong to the beautiful city of Isfahan, is Dougho Gooshfil(Iranian drinks). First of all, it is very surprising for people who have never tried this, how two completely different flavors (salty and sweet) can be combined. Many of those who have resisted trying these two combinations, when they do completely change their minds and even recommend them to others. The name of the sweet (goosh fil) is derived from its resemblance to an elephant’s (fil) ear (goosh). Goosh Fil, as well as Zoolbia and Bamia, is very common and popular in the month of Ramadan.",
+        img: 'Img/Dougho-Gooshfil.JPG'
+    },
+
+
+    {
+        id: 23,
+        name: 'Zulbia',
+        type: 'Dessert',
+        desc: "Zoolbia and Bamiyeh are famous and very popular Iranian sweets that are used as special sweets in the holy month of Ramadan for iftar. Bamieh sweets are very similar to Arabic Legimat sweets. Zoolbia and Bamiya have existed since the time of Naser al-Din Shah. But it is not possible to find the exact date of the origin of Zoolbia and Bamieh. To make Zoolbia you need yogurt, white pastry flour, corn starch, liquid oil, instant yeast, sugar, lukewarm water, and ground saffron.",
+        img: 'Img/Dougho-Gooshfil.JPG'
+    },
+
+    {
+        id: 24,
+        name: 'Dolmeh',
+        type: 'Lunch',
+        desc: "There are many types of dolma, and the most famous ones are grape leaf dolma (Dolmeh Barg Mo), tomato dolma, eggplant, and bell pepper. However, Barge Mo Dolma is more popular and many Iranians prepare this in the spring when the leaves are fresh, sour, and delicious. In Iran, dolmeh is considered one of the local dishes of the West and East Azerbaijan provinces, however, today this delicious dish is cooked all over Iran. The ingredients inside the dolma are a combination of ground meat, dolma vegetables, onions, split beans, etc.",
+        img: 'Img/Dolmeh Barg-e Mo.jpg'
+    },
+
+    {
+        id: 25,
+        name: 'Kufteh Tabrizi',
+        type: 'Dinner',
+        desc: "Kufteh Tabrizi, as its name suggests, is originally from Tabriz, but today it is prepared all over the country. and it is considered one of the most delicious Iranian dishes. Today we have many kinds of Kufteh, but Kufteh Tabrizi is the most famous and delicious. They use a combination of meat, split beans, vegetables, hard-boiled eggs, plums, walnuts, fried onions, and barberry to make the meatballs.",
+        img: 'Img/Dolmeh Barg-e Mo.jpg'
+    },
+
+    {
+        id: 26,
+        name: 'Khoresh Bademjan',
+        type: 'Lunch',
+        desc: "Eggplant (Bademjan) is used in many Iranian dishes and Iranian people love it. One of the most delicious dishes prepared with eggplant is eggplant stew (Khoresh Bademjan). To prepare this stew, they cook meat with onions and spices and then add fried eggplants and lots of sour grapes. Iranians often serve Eggplant stew with white or saffron rice.",
+        img: 'Img/Khoresh Bademjan.JPG'
+    },
+
+    {
+        id: 27,
+        name: 'Kale Pache',
+        type: 'Breakfast',
+        desc: "Kale Pache is a traditional dish in the Middle East and South Caucasus. The cooking method is almost the same in different countries. This dish is a traditional and old food that is still famous among people from the past until today. Early in the morning, many go to the serving stores to buy some or eat it there. Kale Pache is a very energetic and fatty food, and those who have high blood fat or high cholesterol and uric acid in their blood and suffer from gout should avoid eating it. Usually, it takes time to cook this food, and the cook must have good skills to cook it.",
+        img: 'Img/Kale Pache.JPG'
+    },
+
+    {
+        id: 28,
+        name: 'Pashmak',
+        type: 'Dessert',
+        desc: "Pashmak(Persian snack) is a type of sweet that is prepared with simple ingredients. And its texture is in the form of thin strands and fibers that dissolve easily in the mouth. Maybe many suppose that this sweet has no benefits. But because cardamom, flour, vegetable oils, cocoa powder, sugar, saffron, and other additives are in it, it benefits the body.",
+        img: 'Img/Pashmak.JPG'
+    },
+
+    {
+        id: 29,
+        name: 'Qottab',
+        type: 'Dessert',
+        desc: "Qotab is a popular sweet in Iran. It is interesting to know that this sweet is one of the famous sweets of Yazd and Kerman. The Yazdi Qottab consists of two parts, the outer layer, and the inner core. Its outer layer, which is bread fried in oil, consists of wheat flour, milk or yogurt, and egg yolk, which is rolled in powdered sugar after cooking. The inner core of this sweet is made of a mixture of almond powder, sugar powder, and cardamom, ground walnut. In Qutab Asali (Qutab with honey), the main sweetener is honey instead of powdered sugar.",
+        img: 'Img/Qottab.JPG'
+    },
+
+    {
+        id: 30,
+        name: 'Salad Shirazi',
+        type: 'Dessert',
+        desc: "Shirazi Salad is a very delicious combination of cucumber, tomato, and onion, which has a unique taste when seasoned with lemon juice. You can serve it with almost all the dishes you eat with rice. Above all, it is very easy to make. To make this salad, it is enough to finely chop cucumber, tomato, and onion. Then mix all three together and add a little lemon juice, water, and salt to it and serve.In this salad, you can use some chopped parsley or dried mint, as well.It may be interesting to know that instead of using lime juice from the store if you use fresh lime and squeeze it, you will experience a more unique and hearty taste.",
+        img: 'Img/Salad Shirazi.JPG'
+    },
+
+    {
+        id: 31,
+        name: 'Tahdig',
+        type: 'Dessert',
+        desc: "When making the rice, first they boil it and when it is a little soft, they put it in a colander. Now it’s time to make Tahdig. They pour some oil into the pot and add some cardamom, saffron, etc. to the oil and fry a little bit of the rice, bread, or potato depending on what Tahdig they are going to make. For instance, for potato Tahdig they cover the whole bottom of the pot with potato slices and fry them. When the slices are fried and ready, They add the rice on top of the potato slices and leave it on slow heat. All this, in order to make delicious Iranian rice with a yummy Tahdig.",
+        img: 'Img/Tahdig.JPG'
+    },
+
+    {
+        id: 32,
+        name: 'Halim',
+        type: 'Breakfast',
+        desc: "Haleem Gandom (Wheat) is one of the most delicious foods that are in great demand during the months of Muharram and Ramadan. It is also considered an extremely delicious and appropriate Iftar in the month of Ramadan. This delicious dish is usually served for breakfast in other months than Ramadan. You can prepare this food in different ways, but the most common way to prepare Halim is to make it with wheat and bulgur. Red meat, especially lamb or turkey, is preferably used to make delicious Halim. But sometimes some people also prepare it with chicken, which is relatively less tasty.",
+        img: 'Img/Halim.JPG'
+    },
+
+    {
+        id: 33,
+        name: 'Kotlet',
+        type: 'Dinner',
+        desc: "The way to prepare Kotlets is similar to Kuku, with the difference that the minced meat is mixed with grated potatoes and onions, eggs, a little flour, and spices. They take small proportions of the mixture and shape them like a semi-flat circle and fry them. They serve this dish with bread, vegetables, tomato, and pickled cucumber. Meat cutlets, fish cutlets, chicken cutlets, and soy cutlets are among the types of Kotlets.",
+        img: 'Img/Kotlet.JPG'
+    },
+
+    {
+        id: 34,
+        name: 'Sosis Bandari',
+        type: 'Dinner',
+        desc: "Bandari sausage (Sosis) is one of the types of Iranian sandwich foods, whose origin belongs to the southern provinces of our country. This delicious and instant food was gradually transferred from the south of our country to all Iranian cities due to its extremely delicious taste. And today it has become one of the most popular sandwich dishes. The original Bandari sausage is prepared with a combination of sausage, onion and flavorings, but in some restaurants, in addition to these ingredients, they also add mushrooms, cheese slices, etc.",
+        img: 'Img/Sosis Bandari.JPG'
+    },
+
+
+];
 
 export { foods }
